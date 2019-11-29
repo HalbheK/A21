c84046e4259d0b3d1afac60bab8f1284d014cfdf
ITArch User <you@example.com>
2019-11-29 16:41:14 +0100

    Hinzufügen der Readme Datei

---
+++ main.c
@@ -0,0 +1,37 @@
+/*
+ * BlinkALed.c
+ *
+ * Created: 29.11.2019 14:31:00
+ * Author : itarch-user
+ */ 
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB=0x02;
+	DDRD=0x00;
+	PORTD=0x0C;
+
+       while (1) 
+    {
+	if ((PIND& 0x08)==0)
+    {
+		PORTB= 0x02;
+		_delay_ms(1000);
+		PORTB= 0x00;
+		_delay_ms(1000);
+		}
+		else
+		{
+		PORTB=0x02;
+		_delay_ms(500);
+		PORTB=0x00;
+		_delay_ms(500);
+		}
+	 }
+	}
+		
+		
+
